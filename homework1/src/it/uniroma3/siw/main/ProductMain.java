444f2a840fec65ed5957af34b117bc4ed58b028e
Rob-is-the-nickname <92686898+Rob-is-the-nickname@users.noreply.github.com>
2022-04-20 00:24:25 +0200

    Update ProductMain.java

--- homework1/src/it/uniroma3/siw/main/ProductMain.java
+++ homework1/src/it/uniroma3/siw/main/ProductMain.java
@@ -5,27 +5,9 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class ProductMain {
-
-//	public static void main(String[] args) {
-//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("products-unit");
-//		EntityManager em = emf.createEntityManager();
-//		Product product = new Product();
-//		product.setName("KRIDDIG");
-//		product.setPrice(3.5F);
-//		product.setDescription("A wonderful bla bla");
-//		product.setCode("pt000154");
-//
-//		EntityTransaction tx = em.getTransaction();
-//		tx.begin();
-//		em.persist(product);
-//		tx.commit();
-//
-//		em.close();
-//		emf.close();
-//	}
 	
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("products-unit");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("homework-unit");
 		EntityManager em = emf.createEntityManager();
 		em.close();
 		emf.close();
